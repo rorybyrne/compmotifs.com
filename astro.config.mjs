4bc3fe18d493613681bdea74bfe00b0275f5525f
Rory Byrne <rory@rory.bio>
2025-02-15 19:49:55 +0000

    feat: initial Tufte CSS impl

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+    // We need to ensure static files in public/ are being served
+    base: '/', // Ensure we're serving from root
+    outDir: './dist', // Default output directory
+    publicDir: './public', // Explicitly set public directory
+    server: {
+        port: 3000,
+    }
+});
