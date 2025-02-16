734b13f4093641bfa20122e3051dbd76b4e8d5ff
Rory Byrne <rory@rory.bio>
2025-02-16 17:10:38 +0000

    feat: add domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     // We need to ensure static files in public/ are being served
+    site: "https://compmotifs.com",
     base: '/', // Ensure we're serving from root
     outDir: 'dist', // Default output directory
     publicDir: 'public', // Explicitly set public directory
